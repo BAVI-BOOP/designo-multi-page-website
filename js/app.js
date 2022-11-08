81beddba70f6655b9fb34387f7adc041f04d825a
Ahmet Aydin <birceste@gmail.com>
2022-11-08 12:36:28 +0300

    fix

---
+++ js/app.js
@@ -0,0 +1,41 @@
+//! Nav Hamburger Menu
+const hamburgerMenu = document.querySelector(".navMobileMenu");
+const mobileUL = document.querySelector(".mobileUL");
+hamburgerMenu.addEventListener("click", () => {
+  mobileUL.style.display = mobileUL.style.display === "none" ? "block" : "none";
+  setTimeout(() => {
+    mobileUL.style.display = "none";
+  }, 5000);
+});
+
+//! Dark Mode
+
+const darkModeButton = document.querySelector(".lightModeContainer");
+console.log(darkModeButton);
+
+// elements to be changed
+const modeBtn = document.getElementById("btn");
+const haha = document.querySelectorAll("li");
+const navLogo = document.querySelector(".navLogo");
+const navMobileMenu = document.querySelector(".navMobileMenu");
+
+darkModeButton.addEventListener("click", () => {
+  [...haha].forEach(function (el) {
+    modeBtn.checked
+      ? (el.style.color = "#f1f3f5")
+      : (el.style.color = "#1d1c1e");
+  });
+  //
+  [...haha].forEach(function (el) {
+    modeBtn.checked
+      ? el.style.setProperty("--black", "white")
+      : el.style.setProperty("--black", "black");
+  });
+
+  //
+  navLogo.src = modeBtn.checked
+    ? "../assets/shared/desktop/logo-light.png"
+    : "../assets/shared/desktop/logo-dark.png";
+
+  navMobileMenu.style.color = modeBtn.checked ? "#f1f3f5" : "#1d1c1e";
+});
